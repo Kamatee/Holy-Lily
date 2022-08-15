4cdd741eed9d4780d195eb59faafc8e97628f255
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-08-15 14:32:57 +0200

    Death anim add
    Death anim add

--- Holy_Lily/Assets/scripts/PlayerDeath.cs
+++ Holy_Lily/Assets/scripts/PlayerDeath.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -25,9 +27,9 @@ public class PlayerDeath : MonoBehaviour
     }
     private void Die()
     {
+        anim.SetTrigger("death");
         rb.bodyType = RigidbodyType2D.Static;
-        Debug.Log("umiera");
-        RestartLevel();
+        //RestartLevel(); U¿yte w animatorze jako animation event 
     }
 
     

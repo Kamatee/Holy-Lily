a3b99a2c69a69b8e45dc78ab16674f3818a264ee
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-09-04 14:55:17 +0200

    Dragon works
    Dragon works

--- Holy_Lily/Assets/scripts/Dragon/DragonGemMoveSystem.cs
+++ Holy_Lily/Assets/scripts/Dragon/DragonGemMoveSystem.cs
@@ -6,79 +6,31 @@ public class DragonGemMoveSystem : MonoBehaviour, IPointerDownHandler, IBeginDra
 {
     private RectTransform rectTrans;
     public Canvas myCanvas;
+    private CanvasGroup canvasGroup;
+    public int id;
 
     void Start()
     {
         rectTrans = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        Debug.Log("Begin");
+        canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        //Debug.Log("OnDrag");
         rectTrans.anchoredPosition += eventData.delta/myCanvas.scaleFactor;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        Debug.Log("End");
+        canvasGroup.blocksRaycasts = true;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        //throw new System.NotImplementedException();
-        Debug.Log("Start");
     }
-
-    /*public GameObject correctForm;
-private bool moving;
-
-private float startPosX;
-private float startPosY;
-
-void Start()
-{
-
-}
-
-void Update()
-{
-   if (moving)
-   {
-       Vector3 mousePos;
-       mousePos = Input.mousePosition;
-       mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
-       this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX,mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
-   }
-}
-
-private void OnMouseDown()
-{
-   if (Input.GetMouseButtonDown(0))
-   {
-       Vector3 mousePos;
-       mousePos = Input.mousePosition;
-       mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
-       startPosX = mousePos.x = this.transform.localPosition.x;
-       startPosY = mousePos.y = this.transform.localPosition.y;
-
-       moving = true;
-
-   }
-
-}
-
-private void OnMouseUp()
-{
-   moving = false;
-}*/
 
 }
---
+++ Holy_Lily/Assets/scripts/Dragon/SlotScriptDragon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlotScriptDragon : MonoBehaviour, IDropHandler
+{
+    public int id;
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        //sprawdzanie ID
+        if (eventData.pointerDrag.GetComponent<DragonGemMoveSystem>().id == id)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("False");
+        }
+
+        if (eventData.pointerDrag != null)
+        {
+            //uk³ada item w œrodku # nie dzia³a dobrze
+            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}

3154dc886ee5bddf4626bd97bce2bc186fe0dfc5
DamianOrzechowski <72198238+DamianOrzechowski@users.noreply.github.com>
2022-09-06 23:18:11 +0200

    GemsToClickReady

--- Holy_Lily/Assets/scripts/Pickup.cs
+++ Holy_Lily/Assets/scripts/Pickup.cs
@@ -6,15 +6,16 @@ public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
     public GameObject itemButton;
+    public bool playerIsClose;
 
     private void Start()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void Update()
     {
-        if (other.CompareTag("Player"))
+        if (playerIsClose == true && Input.GetKeyDown(KeyCode.E))
         {
             for (int i = 0; i < inventory.slots.Length; i++)
             {
@@ -28,6 +29,41 @@ public class Pickup : MonoBehaviour
                     break;
                 }
             }
+        }
+    }
+
+    /* void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+         {
+             for (int i = 0; i < inventory.slots.Length; i++)
+             {
+                 if (inventory.isFull[i] == false)
+                 {
+                     //item can be added to inventory
+                     inventory.isFull[i] = true;
+                     Instantiate(itemButton, inventory.slots[i].transform, false);
+                     Destroy(gameObject);
+                     GameObject.Find("Dragon").GetComponent<DragonPanel>().GemsToUnlock();
+                     break;
+                 }
+             }
+         }
+     }*/
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerIsClose = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerIsClose = false;
+
         }
     }
 }
